8ae339e8dbbf8ab931e1ead7d8f7879df8ed8290
Debaditya Sen <pabs.sen1@gmail.com>
2024-08-26 04:42:36 +0000

    although video is playing it is crashing the application

--- mediapipe/examples/android/src/java/com/google/mediapipe/apps/basic/CustomGLSurfaceView.java
+++ mediapipe/examples/android/src/java/com/google/mediapipe/apps/basic/CustomGLSurfaceView.java
@@ -2,6 +2,7 @@ package com.google.mediapipe.apps.basic;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.graphics.SurfaceTexture;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -15,6 +16,12 @@ public class CustomGLSurfaceView extends GLSurfaceView {
 
         setEGLContextClientVersion(2);
         mGLRenderer = new GLRenderer() {
+
+            @Override
+            protected Context getContext() {
+                return context;
+            }
+
             @Override
             public void onSurfaceCreated(GL10 gl, EGLConfig config) {
                 super.onSurfaceCreated(gl, config);
@@ -24,6 +31,11 @@ public class CustomGLSurfaceView extends GLSurfaceView {
             public void onSurfaceChanged(GL10 gl, int width, int height) {
                 // Override this method if needed
             }
+            @Override
+            public void onFrameAvailable(SurfaceTexture surfaceTexture) {
+                // Handle the frame available event here
+            }
+            
         };
         setRenderer(mGLRenderer);
     }
--- mediapipe/examples/android/src/java/com/google/mediapipe/apps/basic/GLRenderer.java
+++ mediapipe/examples/android/src/java/com/google/mediapipe/apps/basic/GLRenderer.java
@@ -2,6 +2,11 @@ package com.google.mediapipe.apps.basic;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.graphics.SurfaceTexture;
+import android.opengl.GLUtils;
+import android.media.MediaPlayer;
+import android.content.Context;
+import android.view.Surface;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -10,32 +15,48 @@ import java.nio.FloatBuffer;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-public abstract class GLRenderer implements GLSurfaceView.Renderer {
+public abstract class GLRenderer implements GLSurfaceView.Renderer, SurfaceTexture.OnFrameAvailableListener {
 
     private FloatBuffer vertexBuffer;
+    private FloatBuffer textureBuffer;
     private final String vertexShaderCode =
         "attribute vec4 vPosition;" +
+        "attribute vec2 aTexCoord;" +
+        "varying vec2 vTexCoord;" +
         "void main() {" +
         "  gl_Position = vPosition;" +
+        "  vTexCoord = aTexCoord;" +
         "}";
     private final String fragmentShaderCode =
         "precision mediump float;" +
-        "uniform vec4 vColor;" +
+        "uniform sampler2D uTexture;" +
+        "varying vec2 vTexCoord;" +
         "void main() {" +
-        "  gl_FragColor = vColor;" +
+        "  gl_FragColor = texture2D(uTexture, vTexCoord);" +
         "}";
     private int mProgram;
     private int positionHandle;
-    private int colorHandle;
+    private int textureHandle;
+    private int textureId;
+    private SurfaceTexture surfaceTexture;
+    private MediaPlayer mediaPlayer;
     private final int vertexCount = 4;
-    private final int vertexStride = 3 * 4;
-    
-    float vertexCoordinates[] = {
+    private final int vertexStride = 0;
+
+    private float vertexCoordinates[] = {
         -0.5f,  0.25f, 0.0f,   // top left
          0.5f,  0.25f, 0.0f,   // top right
         -0.5f, -0.25f, 0.0f,   // bottom left
          0.5f, -0.25f, 0.0f    // bottom right
     };
+
+    private float[] textureCoordinates = {
+        0.0f, 0.0f,  // top left
+        1.0f, 0.0f,  // top right
+        0.0f, 1.0f,  // bottom left
+        1.0f, 1.0f   // bottom right
+    };
+
     float color[] = { 1.0f, 0.0f, 0.0f, 1.0f }; // Red color
 
     public void setPlaneCoordinates(float[] coordinates) {
@@ -56,6 +77,14 @@ public abstract class GLRenderer implements GLSurfaceView.Renderer {
         vertexBuffer.put(vertexCoordinates);
         vertexBuffer.position(0);
 
+        // Setup texture Buffer
+        ByteBuffer tb = ByteBuffer.allocateDirect(textureCoordinates.length * 4);
+        tb.order(ByteOrder.nativeOrder());
+        textureBuffer = tb.asFloatBuffer();
+        textureBuffer.put(textureCoordinates);
+        textureBuffer.position(0);
+
+        // Complie the shader and link program
         int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
         int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
 
@@ -63,21 +92,59 @@ public abstract class GLRenderer implements GLSurfaceView.Renderer {
         GLES20.glAttachShader(mProgram, vertexShader);
         GLES20.glAttachShader(mProgram, fragmentShader);
         GLES20.glLinkProgram(mProgram);
+
+        // Set up texture
+        int[] textures = new int[1];
+        GLES20.glGenTextures(1, textures, 0);
+        textureId = textures[0];
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+
+        surfaceTexture = new SurfaceTexture(textureId);
+        surfaceTexture.setOnFrameAvailableListener(this);
+
+        // Set up MediaPlayer
+        mediaPlayer = MediaPlayer.create(getContext(), R.raw.test);
+        mediaPlayer.setSurface(new Surface(surfaceTexture));
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
     }
 
     public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
         GLES20.glUseProgram(mProgram);
 
+        // Update Texture with the video frame
+        surfaceTexture.updateTexImage();
+
+        //Pass vertex data to shader
         positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
         GLES20.glEnableVertexAttribArray(positionHandle);
-        GLES20.glVertexAttribPointer(positionHandle, 3, GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
+        GLES20.glVertexAttribPointer(positionHandle, 3, GLES20.GL_FLOAT, false, 0, vertexBuffer);
 
-        colorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
-        GLES20.glUniform4fv(colorHandle, 1, color, 0);
+        // Pass texture coordinates data to shader
+        textureHandle = GLES20.glGetAttribLocation(mProgram, "aTexCoord");
+        GLES20.glEnableVertexAttribArray(textureHandle);
+        GLES20.glVertexAttribPointer(textureHandle, 2, GLES20.GL_FLOAT, false, 0, textureBuffer);
 
+        // Bind the texture
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
+
+        // Draw the plane
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, vertexCount);
 
+        // disable attribute arrays
         GLES20.glDisableVertexAttribArray(positionHandle);
+        GLES20.glDisableVertexAttribArray(textureHandle);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
     }
 
     private int loadShader(int type, String shaderCode) {
@@ -86,4 +153,6 @@ public abstract class GLRenderer implements GLSurfaceView.Renderer {
         GLES20.glCompileShader(shader);
         return shader;
     }
+
+    protected abstract Context getContext();
 }
