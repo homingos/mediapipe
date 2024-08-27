94d7ff207bc4f4d54f68b475a843719b82323c15
Debaditya Sen <pabs.sen1@gmail.com>
2024-08-27 10:47:28 +0000

    added screen util

---
+++ mediapipe/examples/android/src/java/com/google/mediapipe/apps/basic/ScreenUtils.java
@@ -0,0 +1,21 @@
+package com.google.mediapipe.apps.basic;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+public class ScreenUtils {
+    public static int getScreenWidth(Context context) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
+    }
+
+    public static int getScreenHeight(Context context) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.heightPixels;
+    }
+}
